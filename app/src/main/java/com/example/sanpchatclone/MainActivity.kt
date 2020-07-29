34732338f241460e527489c6afc8431ced339c3e
kanhaiya <ikanhaiyasharma@gmail.com>
2020-07-29 19:29:18 +0530

    firebase signup login complete

--- app/src/main/java/com/example/sanpchatclone/MainActivity.kt
+++ app/src/main/java/com/example/sanpchatclone/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.sanpchatclone
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.password
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
 
 
     var emailEditText : EditText? = null;
     var passwordEditText : EditText? = null;
+    val mAuth = FirebaseAuth.getInstance()
+
+
 
 
 
@@ -20,15 +29,64 @@ class MainActivity : AppCompatActivity() {
 emailEditText = findViewById(R.id.emailfield);
     passwordEditText = findViewById(R.id.passwordfield);
 
+        if (mAuth.currentUser!=null){
+
+           logIn()
+
+
+
+
+        }
+
+
+
+
     }
 
 
     fun goClicked(view: View){
 
+// check if we can log in the user
+// sign up the user
 
+
+        mAuth.signInWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString())
+            .addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    logIn()
+                } else {
+                    mAuth.createUserWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString()).addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                               logIn()
+
+                            } else {
+                               Toast.makeText(this,"Login failed ! Please try again",Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+
+                }
+
+                // ...
+            }
 
 
     }
+
+fun logIn(){
+
+// move to next activity
+val intent = Intent(this, SanpsActivity::class.java)
+startActivity(intent)
+}
+
+
+
+
+
 
 
 
